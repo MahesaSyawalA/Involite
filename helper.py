66156812e463b4a07e1cc3a86574f6a7b13a8524
MahesaSyawalA <mahesaabd4@gmail.com>
2025-01-03 19:16:00 +0700

    add handling for validate input menu

--- helper.py
+++ helper.py
@@ -18,27 +18,33 @@ def save_data(data):
         json.dump(data, file, indent=4)
 
 
-def validate_input(prompt, pattern, cast_type=str, default=None, validate_date=False):
+def validate_input(prompt, pattern, cast_type=str, default=None, validate_date=False, menu=False, maxMenu=0):
     """Validate user input against a regex pattern and additional checks like date validation."""
     while True:
         user_input = input(prompt)
         if not user_input and default is not None:
             return default
         
-        # Validasi format menggunakan regex
         if re.match(pattern, user_input):
-            # Jika validate_date True, lakukan validasi tanggal lebih dari hari ini
             if validate_date:
                 try:
                     input_date = datetime.strptime(user_input, "%Y-%m-%d").date()
                     today = datetime.today().date()
                     if input_date > today:
                         print("Tanggal tidak boleh lebih dari tanggal hari ini!")
-                        continue  # Minta input ulang
+                        continue  
                 except ValueError:
                     print("Tanggal tidak valid! Periksa kembali format atau tanggal yang dimasukkan.")
-                    continue  # Minta input ulang
+                    continue  
 
+            if menu:
+                try:
+                    if int(user_input) > maxMenu:
+                        print('inputan anda melebihi menu yang ada, silakan input kembali')
+                        continue
+                except ValueError:
+                    print('inputan tidak valid, silakan coba lagi')
+                    continue
             return cast_type(user_input)
         
         print("Input tidak valid. Silakan coba lagi.")
